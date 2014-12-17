ce71f329c517714f07427f544e27c80e6c4dfde1
Roman Birg <roman@cyngn.com>
2014-12-18 00:19:27 +0200

    Settings: add volume tone toggle back [2/2]
    This patch allows the users to choose if they want the audible tone when adjusting the volume
    
    PureNexus Edits:
    *update for 8.1 and add a preference controller
    
    [POSP: Kshitij Gupta]
    - Adapt to the new code-style
    - Disable by default
    
    Change-Id: I84e2085b9be0a87ddfdbd5175ae6b218c943922c
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -293,6 +293,8 @@ public class SoundSettings extends DashboardFragment {
                 new TouchSoundPreferenceController(context, fragment, lifecycle);
         final VibrateOnTouchPreferenceController vibrateOnTouchPreferenceController =
                 new VibrateOnTouchPreferenceController(context, fragment, lifecycle);
+        final VolumeAdjustmentTonePreferenceController volumeAdjustmentPreferenceController =
+                new VolumeAdjustmentTonePreferenceController(context, fragment, lifecycle);
         final DockAudioMediaPreferenceController dockAudioMediaPreferenceController =
                 new DockAudioMediaPreferenceController(context, fragment, lifecycle);
         final BootSoundPreferenceController bootSoundPreferenceController =
@@ -309,6 +311,7 @@ public class SoundSettings extends DashboardFragment {
         controllers.add(dockingSoundPreferenceController);
         controllers.add(touchSoundPreferenceController);
         controllers.add(vibrateOnTouchPreferenceController);
+        controllers.add(volumeAdjustmentPreferenceController);
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
