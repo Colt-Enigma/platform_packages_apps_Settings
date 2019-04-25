nsytechno/packages_apps_Settings

SystemUpdatePreferenceController: Refactor for our OTA app
Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

src/com/android/settings/deviceinfo/SystemUpdatePreferenceController.java
@@ -16,22 +16,17 @@
 package com.android.settings.deviceinfo;
 
 import android.content.Context;
-import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.PersistableBundle;
 import android.os.UserManager;
+import android.os.SystemProperties;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.telephony.CarrierConfigManager;
-import android.text.TextUtils;
-import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
-
-import static android.content.Context.CARRIER_CONFIG_SERVICE;
 
 public class SystemUpdatePreferenceController extends AbstractPreferenceController implements
         PreferenceControllerMixin {
@@ -42,6 +37,9 @@ public class SystemUpdatePreferenceController extends AbstractPreferenceControll
 
     private final UserManager mUm;
 
+    private static final String OTA_BUILD_TYPE_PROP = "org.pixelexperience.build_type";
+    private static final String OTA_APP_PACKAGE = "org.pixelexperience.ota";
+
     public SystemUpdatePreferenceController(Context context, UserManager um) {
         super(context);
         mUm = um;
@@ -49,7 +47,17 @@ public class SystemUpdatePreferenceController extends AbstractPreferenceControll
 
     @Override
     public boolean isAvailable() {
-        return false;
+        String buildtype = SystemProperties.get(OTA_BUILD_TYPE_PROP,"unofficial");
+        if (!mUm.isAdminUser() || !buildtype.equalsIgnoreCase("official")){
+            return false;
+        }
+        try {
+            PackageManager pm = mContext.getPackageManager();
+            pm.getPackageInfo(OTA_APP_PACKAGE, PackageManager.GET_ACTIVITIES);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -71,12 +79,7 @@ public class SystemUpdatePreferenceController extends AbstractPreferenceControll
     @Override
     public boolean handlePreferenceTreeClick(Preference preference) {
         if (KEY_SYSTEM_UPDATE_SETTINGS.equals(preference.getKey())) {
-            CarrierConfigManager configManager =
-                    (CarrierConfigManager) mContext.getSystemService(CARRIER_CONFIG_SERVICE);
-            PersistableBundle b = configManager.getConfig();
-            if (b != null && b.getBoolean(CarrierConfigManager.KEY_CI_ACTION_ON_SYS_UPDATE_BOOL)) {
-                ciActionOnSysUpdate(b);
-            }
+            // TODO: Launch Pixel Experience OTA
         }
         // always return false here because this handler does not want to block other handlers.
         return false;
@@ -86,27 +89,5 @@ public class SystemUpdatePreferenceController extends AbstractPreferenceControll
     public void updateState(Preference preference) {
         preference.setSummary(mContext.getString(R.string.about_summary,
                 Build.VERSION.RELEASE));
-    }
-
-    /**
-     * Trigger client initiated action (send intent) on system update
-     */
-    private void ciActionOnSysUpdate(PersistableBundle b) {
-        String intentStr = b.getString(CarrierConfigManager.
-                KEY_CI_ACTION_ON_SYS_UPDATE_INTENT_STRING);
-        if (!TextUtils.isEmpty(intentStr)) {
-            String extra = b.getString(CarrierConfigManager.
-                    KEY_CI_ACTION_ON_SYS_UPDATE_EXTRA_STRING);
-            String extraVal = b.getString(CarrierConfigManager.
-                    KEY_CI_ACTION_ON_SYS_UPDATE_EXTRA_VAL_STRING);
-
-            Intent intent = new Intent(intentStr);
-            if (!TextUtils.isEmpty(extra)) {
-                intent.putExtra(extra, extraVal);
-            }
-            Log.d(TAG, "ciActionOnSysUpdate: broadcasting intent " + intentStr +
-                    " with extra " + extra + ", " + extraVal);
-            mContext.getApplicationContext().sendBroadcast(intent);
-        }
     }
 }

Colt-Enigma/platform_packages_apps_Settings

Change-Id: I5020b0094ecf8b8872068e1137dfaea5eb702461
Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -15,50 +15,46 @@
  */
 package com.android.settings.system;
 
-import static android.content.Context.CARRIER_CONFIG_SERVICE;
-import static android.content.Context.SYSTEM_UPDATE_SERVICE;
-
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.os.SystemUpdateManager;
+import android.content.pm.PackageManager;
 import android.os.UserManager;
+import android.os.SystemProperties;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.telephony.CarrierConfigManager;
-import android.text.TextUtils;
-import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
-
 public class SystemUpdatePreferenceController extends BasePreferenceController {
-
-    private static final String TAG = "SysUpdatePrefContr";
 
     private static final String KEY_SYSTEM_UPDATE_SETTINGS = "system_update_settings";
 
+    private static final String OTA_BUILD_TYPE_PROP = "com.colt.build_type";
+    private static final String DEVICE_NAME = "com.colt.device";
+    private static final String OTA_APP_PACKAGE = "com.colt.ota";
+
     private final UserManager mUm;
-    private final SystemUpdateManager mUpdateManager;
 
     public SystemUpdatePreferenceController(Context context) {
         super(context, KEY_SYSTEM_UPDATE_SETTINGS);
         mUm = UserManager.get(context);
-        mUpdateManager = (SystemUpdateManager) context.getSystemService(SYSTEM_UPDATE_SERVICE);
     }
 
     @Override
     public int getAvailabilityStatus() {
-        return mContext.getResources().getBoolean(R.bool.config_show_system_update_settings)
-                && mUm.isAdminUser()
-                ? AVAILABLE
-                : UNSUPPORTED_ON_DEVICE;
+        String buildtype = SystemProperties.get(OTA_BUILD_TYPE_PROP,"unofficial");
+        String deviceName = SystemProperties.get(DEVICE_NAME,"");
+        if (deviceName.startsWith("phhgsi_") || !mUm.isAdminUser() || !buildtype.equalsIgnoreCase("official")){
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        try {
+            PackageManager pm = mContext.getPackageManager();
+            pm.getPackageInfo(OTA_APP_PACKAGE, PackageManager.GET_ACTIVITIES);
+        } catch (Exception e) {
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        return AVAILABLE;
     }
 
     @Override
@@ -73,72 +69,12 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
 
     @Override
     public boolean handlePreferenceTreeClick(Preference preference) {
-        if (TextUtils.equals(getPreferenceKey(), preference.getKey())) {
-            CarrierConfigManager configManager =
-                    (CarrierConfigManager) mContext.getSystemService(CARRIER_CONFIG_SERVICE);
-            PersistableBundle b = configManager.getConfig();
-            if (b != null && b.getBoolean(CarrierConfigManager.KEY_CI_ACTION_ON_SYS_UPDATE_BOOL)) {
-                ciActionOnSysUpdate(b);
-            }
-        }
-        // always return false here because this handler does not want to block other handlers.
         return false;
     }
 
     @Override
     public CharSequence getSummary() {
-        CharSequence summary = mContext.getString(R.string.android_version_summary,
-                Build.VERSION.RELEASE);
-        final FutureTask<Bundle> bundleFutureTask = new FutureTask<>(
-                // Put the API call in a future to avoid StrictMode violation.
-                () -> mUpdateManager.retrieveSystemUpdateInfo());
-        final Bundle updateInfo;
-        try {
-            bundleFutureTask.run();
-            updateInfo = bundleFutureTask.get();
-        } catch (InterruptedException | ExecutionException e) {
-            Log.w(TAG, "Error getting system update info.");
-            return summary;
-        }
-        switch (updateInfo.getInt(SystemUpdateManager.KEY_STATUS)) {
-            case SystemUpdateManager.STATUS_WAITING_DOWNLOAD:
-            case SystemUpdateManager.STATUS_IN_PROGRESS:
-            case SystemUpdateManager.STATUS_WAITING_INSTALL:
-            case SystemUpdateManager.STATUS_WAITING_REBOOT:
-                summary = mContext.getText(R.string.android_version_pending_update_summary);
-                break;
-            case SystemUpdateManager.STATUS_UNKNOWN:
-                Log.d(TAG, "Update statue unknown");
-                // fall through to next branch
-            case SystemUpdateManager.STATUS_IDLE:
-                final String version = updateInfo.getString(SystemUpdateManager.KEY_TITLE);
-                if (!TextUtils.isEmpty(version)) {
-                    summary = mContext.getString(R.string.android_version_summary, version);
-                }
-                break;
-        }
+        CharSequence summary = mContext.getString(R.string.summary_empty);
         return summary;
-    }
-
-    /**
-     * Trigger client initiated action (send intent) on system update
-     */
-    private void ciActionOnSysUpdate(PersistableBundle b) {
-        String intentStr = b.getString(CarrierConfigManager.
-                KEY_CI_ACTION_ON_SYS_UPDATE_INTENT_STRING);
-        if (!TextUtils.isEmpty(intentStr)) {
-            String extra = b.getString(CarrierConfigManager.
-                    KEY_CI_ACTION_ON_SYS_UPDATE_EXTRA_STRING);
-            String extraVal = b.getString(CarrierConfigManager.
-                    KEY_CI_ACTION_ON_SYS_UPDATE_EXTRA_VAL_STRING);
-
-            Intent intent = new Intent(intentStr);
-            if (!TextUtils.isEmpty(extra)) {
-                intent.putExtra(extra, extraVal);
-            }
-            Log.d(TAG, "ciActionOnSysUpdate: broadcasting intent " + intentStr +
-                    " with extra " + extra + ", " + extraVal);
-            mContext.getApplicationContext().sendBroadcast(intent);
-        }
     }
 }