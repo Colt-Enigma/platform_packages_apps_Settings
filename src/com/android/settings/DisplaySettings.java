83c94c3ae24bd2c0794557e2c77d639aa636ee32
SuperDroidBond <superdroidbond@yahoo.com>
2020-01-07 22:52:19 +0530

    Remove AOSP Accent Picker

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -96,8 +96,6 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
-	controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.accent_color"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.font"));
         controllers.add(new OverlayCategoryPreferenceController(context,
