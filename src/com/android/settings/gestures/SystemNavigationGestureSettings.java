eec7368ff034d81aa0ee45ac9ef68aa54c193dcb
Anushek Prasal <anushekprasal@gmail.com>
2020-03-24 02:27:03 +0530

    Settings: Improve gesture navigation handle length [2/3]
    Change-Id: I7f1564361ab59e7cc2a27bf60f7719d2ea930fdc

--- src/com/android/settings/gestures/SystemNavigationGestureSettings.java
+++ src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -298,22 +298,13 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment {
     }
 
     static void setHomeHandleSize(Context context, int length) {
-        // length cant be range 0 - 2
-        // 2 means long
-        // 1 measns middle
-        // 0 means aosp size
         Settings.System.putInt(context.getContentResolver(),
                 Settings.System.NAVIGATION_HANDLE_WIDTH, length);
     }
 
     static int getHomeHandleSize(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
-                Settings.System.NAVIGATION_HANDLE_WIDTH, 0);
-    }
-
-    static int getPillToggleState(Context context) {
-        return Settings.System.getInt(context.getContentResolver(),
-                Settings.System.GESTURE_PILL_TOGGLE, 0);
+                Settings.System.NAVIGATION_HANDLE_WIDTH, 1);
     }
 
     @VisibleForTesting
