Project-Xtended/packages_apps_Settings

Remove AOSP Ambient display conditionally

src/com/android/settings/display/AmbientDisplayAlwaysOnPreferenceController.java
@@ -23,6 +23,7 @@ import android.provider.Settings;
 import android.text.TextUtils;
 
 import com.android.settings.core.TogglePreferenceController;
+import com.android.internal.util.xtended.XtendedUtils;
 
 public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreferenceController {
 
@@ -46,6 +47,7 @@ public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreference
     @Override
     public int getAvailabilityStatus() {
         return isAvailable(getConfig())
+                && !XtendedUtils.hasAltAmbientDisplay(mContext.getApplicationContext())
                 && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false) ?
                 AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }

src/com/android/settings/display/AmbientDisplayNotificationsPreferenceController.java
@@ -29,6 +29,8 @@ import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
 
+import com.android.internal.util.xtended.XtendedUtils;
+
 public class AmbientDisplayNotificationsPreferenceController extends
         TogglePreferenceController implements Preference.OnPreferenceChangeListener {
 
@@ -79,7 +81,8 @@ public class AmbientDisplayNotificationsPreferenceController extends
 
     @Override
     public int getAvailabilityStatus() {
-        return getAmbientConfig().pulseOnNotificationAvailable()
+        return !XtendedUtils.hasAltAmbientDisplay(mContext.getApplicationContext()) &&
+               getAmbientConfig().pulseOnNotificationAvailable()
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

Colt-Enigma/platform_packages_apps_Settings

Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

src/com/android/settings/display/AmbientDisplayAlwaysOnPreferenceController.java
@@ -23,6 +23,7 @@ import android.provider.Settings;
 import android.text.TextUtils;
 
 import com.android.settings.core.TogglePreferenceController;
+import com.android.internal.util.colt.ColtUtils;
 
 public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreferenceController {
 
@@ -46,6 +47,7 @@ public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreference
     @Override
     public int getAvailabilityStatus() {
         return isAvailable(getConfig())
+                && !ColtUtils.hasAltAmbientDisplay(mContext.getApplicationContext())
                 && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false) ?
                 AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }

src/com/android/settings/display/AmbientDisplayNotificationsPreferenceController.java
@@ -29,6 +29,8 @@ import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
 
+import com.android.internal.util.colt.ColtUtils;
+
 public class AmbientDisplayNotificationsPreferenceController extends
         TogglePreferenceController implements Preference.OnPreferenceChangeListener {
 
@@ -79,7 +81,8 @@ public class AmbientDisplayNotificationsPreferenceController extends
 
     @Override
     public int getAvailabilityStatus() {
-        return getAmbientConfig().pulseOnNotificationAvailable()
+        return !ColtUtils.hasAltAmbientDisplay(mContext.getApplicationContext()) &&
+               getAmbientConfig().pulseOnNotificationAvailable()
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 