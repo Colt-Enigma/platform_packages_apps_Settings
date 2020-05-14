omnirom/android_packages_apps_Settings

Developer opts: set default notification channel warnings
to DISABLED when developer options are enabled

Change-Id: I8bbcccb0cfe187f637555053a3d8073463f05c99

src/com/android/settings/development/NotificationChannelWarningsPreferenceController.java
@@ -63,7 +63,7 @@ public class NotificationChannelWarningsPreferenceController extends
 
     @Override
     public void updateState(Preference preference) {
-        final int defaultWarningEnabled = isDebuggable() ? DEBUGGING_ENABLED : DEBUGGING_DISABLED;
+        final int defaultWarningEnabled = DEBUGGING_DISABLED;
         final int mode = Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.SHOW_NOTIFICATION_CHANNEL_WARNINGS, defaultWarningEnabled);
         ((SwitchPreference) mPreference).setChecked(mode != SETTING_VALUE_OFF);

Colt-Enigma/platform_packages_apps_Settings

Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

src/com/android/settings/development/NotificationChannelWarningsPreferenceController.java
@@ -64,7 +64,7 @@ public class NotificationChannelWarningsPreferenceController extends
 
     @Override
     public void updateState(Preference preference) {
-        final int defaultWarningEnabled = isDebuggable() ? DEBUGGING_ENABLED : DEBUGGING_DISABLED;
+        final int defaultWarningEnabled = SETTING_VALUE_OFF;
         final int mode = Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.SHOW_NOTIFICATION_CHANNEL_WARNINGS, defaultWarningEnabled);
         ((SwitchPreference) mPreference).setChecked(mode != SETTING_VALUE_OFF);