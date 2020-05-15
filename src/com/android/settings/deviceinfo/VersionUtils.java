fb572ef873586f574179319d56ea257836f617b6
Mrinal Ghosh <mg712702@gmail.com>
2020-05-15 19:42:54 +0530

    Settigs: Fix colt stuffs
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/android/settings/deviceinfo/VersionUtils.java
+++ src/com/android/settings/deviceinfo/VersionUtils.java
@@ -5,7 +5,7 @@ import android.os.SystemProperties;
 
 public class VersionUtils {
     public static String getColtVersion(){
-        String buildType = SystemProperties.get("ro.colt.build.version","");
+        String buildType = SystemProperties.get("ro.colt.version","");
 	return buildType.equals("") ? "" : buildType;
     }
 }
