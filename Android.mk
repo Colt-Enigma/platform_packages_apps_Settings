DirtyUnicorns/android_packages_apps_Settings

Import kotlin dependencies
- Import expandable bottom bar lib with latest commits as of
1.1.6 on the 'master' branch

https://github.com/st235/ExpandableBottomBar

- Update Navbar fragments filepaths

Change-Id: Ie1ed7afd51201f9c284343c6fa167fe1879a8f4a

Android.mk
@@ -54,7 +54,9 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     settings-log-bridge-protos-lite \
     contextualcards \
     settings-logtags \
-    zxing-core-1.7
+    zxing-core-1.7 \
+    expandablebottombar \
+    kotlin-stdlib
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     packages/apps/DU-Changelog/app/src/main/res \
@@ -70,7 +72,8 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages androidx.recyclerview_recyclerview \
     --extra-packages androidx.design_design \
     --extra-packages com.dirtyunicorns.changelog \
-    --extra-packages com.dirtyunicorns.tweaks
+    --extra-packages com.dirtyunicorns.tweaks \
+    --extra-packages github.com.st235.lib_expandablebottombar
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
@@ -87,7 +90,9 @@ include $(BUILD_PACKAGE)
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    contextualcards:libs/contextualcards.aar
+    contextualcards:libs/contextualcards.aar \
+    expandablebottombar:libs/expandable_bottom_bar_116.aar
+
 include $(BUILD_MULTI_PREBUILT)
 
 # Use the following include to make our test apk.

Colt-Enigma/platform_packages_apps_Settings

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

Android.mk
@@ -54,7 +54,9 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     settings-logtags \
     zxing-core-1.7 \
     glide \
-    airbnb-lottie
+    airbnb-lottie \
+    expandablebottombar \
+    kotlin-stdlib
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     packages/apps/ColtCenter/res
@@ -62,7 +64,8 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages com.colt.settings
+    --extra-packages com.colt.settings \
+    --extra-packages github.com.st235.lib_expandablebottombar
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
@@ -79,7 +82,9 @@ include $(BUILD_PACKAGE)
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    contextualcards:libs/contextualcards.aar
+    contextualcards:libs/contextualcards.aar \
+    expandablebottombar:libs/expandable_bottom_bar_116.aar
+
 include $(BUILD_MULTI_PREBUILT)
 
 # Use the following include to make our test apk.