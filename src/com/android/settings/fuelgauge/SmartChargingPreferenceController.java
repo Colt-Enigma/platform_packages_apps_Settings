Havoc-OS/android_packages_apps_Settings

Settings: SmartCharging: Use Master Switch
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

src/com/android/settings/fuelgauge/SmartChargingPreferenceController.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.android.settings.fuelgauge;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import com.havoc.support.preferences.SystemSettingMasterSwitchPreference;
+
+public class SmartChargingPreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_SMART_CHARGING = "smart_charging";
+
+    private SystemSettingMasterSwitchPreference mSmartCharging;
+
+    public SmartChargingPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_SMART_CHARGING;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        mSmartCharging = (SystemSettingMasterSwitchPreference) screen.findPreference(KEY_SMART_CHARGING);
+        mSmartCharging.setChecked((Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SMART_CHARGING, 0) == 1));
+        mSmartCharging.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value = (Boolean) newValue;
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.SMART_CHARGING, value ? 1 : 0);
+        return true;
+    }
+}

src/com/android/settings/fuelgauge/smartcharging/SmartChargingSettings.java
@@ -21,36 +21,47 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 
+import com.havoc.support.preferences.CustomSeekBarPreference;
+import com.havoc.support.preferences.SystemSettingSwitchPreference;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import com.havoc.support.preferences.CustomSeekBarPreference;
 
 /**
  * Settings screen for Smart charging
  */
-public class SmartChargingSettings extends DashboardFragment implements OnPreferenceChangeListener {
+public class SmartChargingSettings extends DashboardFragment implements
+        OnPreferenceChangeListener, CompoundButton.OnCheckedChangeListener {
 
     private static final String TAG = "SmartChargingSettings";
     private static final String KEY_SMART_CHARGING_LEVEL = "smart_charging_level";
     private static final String KEY_SMART_CHARGING_RESUME_LEVEL = "smart_charging_resume_level";
     private CustomSeekBarPreference mSmartChargingLevel;
     private CustomSeekBarPreference mSmartChargingResumeLevel;
+    private SystemSettingSwitchPreference mResetStats;
 
     private int mSmartChargingLevelDefaultConfig;
     private int mSmartChargingResumeLevelDefaultConfig;
+
+    private TextView mTextView;
+    private View mSwitchBar;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -74,7 +85,55 @@ public class SmartChargingSettings extends DashboardFragment implements OnPrefer
         mSmartChargingResumeLevel.setValue(currentResumeLevel);
         mSmartChargingResumeLevel.setOnPreferenceChangeListener(this);
 
-        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.smart_charging_footer);
+        mResetStats = (SystemSettingSwitchPreference) findPreference("smart_charging_reset_stats");
+
+        //mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.smart_charging_footer);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.master_setting_switch, container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        boolean enabled = Settings.System.getInt(getContentResolver(),
+                Settings.System.SMART_CHARGING, 0) == 1;
+
+        mTextView = view.findViewById(R.id.switch_text);
+        mTextView.setText(getString(enabled ?
+                R.string.switch_on_text : R.string.switch_off_text));
+
+        mSwitchBar = view.findViewById(R.id.switch_bar);
+        Switch switchWidget = mSwitchBar.findViewById(android.R.id.switch_widget);
+        switchWidget.setChecked(enabled);
+        switchWidget.setOnCheckedChangeListener(this);
+        mSwitchBar.setActivated(enabled);
+        mSwitchBar.setOnClickListener(v -> {
+            switchWidget.setChecked(!switchWidget.isChecked());
+            mSwitchBar.setActivated(switchWidget.isChecked());
+        });
+
+        mSmartChargingLevel.setEnabled(enabled);
+        mSmartChargingResumeLevel.setEnabled(enabled);
+        mResetStats.setEnabled(enabled);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.SMART_CHARGING, isChecked ? 1 : 0);
+        mTextView.setText(getString(isChecked ? R.string.switch_on_text : R.string.switch_off_text));
+        mSwitchBar.setActivated(isChecked);
+
+        mSmartChargingLevel.setEnabled(isChecked);
+        mSmartChargingResumeLevel.setEnabled(isChecked);
+        mResetStats.setEnabled(isChecked);
     }
 
     @Override
@@ -119,18 +178,4 @@ public class SmartChargingSettings extends DashboardFragment implements OnPrefer
             return false;
         }
     }
-
-    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                        boolean enabled) {
-                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
-
-                    final SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.smart_charging;
-                    result.add(sir);
-                    return result;
-                }
-            };
 }

Colt-Enigma/platform_packages_apps_Settings

Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

src/com/android/settings/fuelgauge/SmartChargingPreferenceController.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.android.settings.fuelgauge;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import com.colt.enigma.preference.SystemSettingMasterSwitchPreference;
+
+public class SmartChargingPreferenceController extends AbstractPreferenceController implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_SMART_CHARGING = "smart_charging";
+
+    private SystemSettingMasterSwitchPreference mSmartCharging;
+
+    public SmartChargingPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_SMART_CHARGING;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        mSmartCharging = (SystemSettingMasterSwitchPreference) screen.findPreference(KEY_SMART_CHARGING);
+        mSmartCharging.setChecked((Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SMART_CHARGING, 0) == 1));
+        mSmartCharging.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value = (Boolean) newValue;
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.SMART_CHARGING, value ? 1 : 0);
+        return true;
+    }
+}

src/com/android/settings/fuelgauge/smartcharging/SmartChargingSettings.java
@@ -21,13 +21,18 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
-import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
 import com.android.settingslib.core.lifecycle.Lifecycle;
@@ -36,20 +41,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.colt.enigma.preference.CustomSeekBarPreference;
+import com.colt.enigma.preference.SystemSettingSwitchPreference;
 
 /**
  * Settings screen for Smart charging
  */
-public class SmartChargingSettings extends DashboardFragment implements OnPreferenceChangeListener {
+public class SmartChargingSettings extends DashboardFragment implements
+        OnPreferenceChangeListener, CompoundButton.OnCheckedChangeListener {
 
     private static final String TAG = "SmartChargingSettings";
     private static final String KEY_SMART_CHARGING_LEVEL = "smart_charging_level";
     private static final String KEY_SMART_CHARGING_RESUME_LEVEL = "smart_charging_resume_level";
     private CustomSeekBarPreference mSmartChargingLevel;
     private CustomSeekBarPreference mSmartChargingResumeLevel;
+    private SystemSettingSwitchPreference mResetStats;
 
     private int mSmartChargingLevelDefaultConfig;
     private int mSmartChargingResumeLevelDefaultConfig;
+
+    private TextView mTextView;
+    private View mSwitchBar;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -71,6 +83,56 @@ public class SmartChargingSettings extends DashboardFragment implements OnPrefer
         if (currentResumeLevel >= currentLevel) currentResumeLevel = currentLevel -1;
         mSmartChargingResumeLevel.setValue(currentResumeLevel);
         mSmartChargingResumeLevel.setOnPreferenceChangeListener(this);
+
+        mResetStats = (SystemSettingSwitchPreference) findPreference("smart_charging_reset_stats");
+
+        //mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.smart_charging_footer);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.master_setting_switch, container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        boolean enabled = Settings.System.getInt(getContentResolver(),
+                Settings.System.SMART_CHARGING, 0) == 1;
+
+        mTextView = view.findViewById(R.id.switch_text);
+        mTextView.setText(getString(enabled ?
+                R.string.switch_on_text : R.string.switch_off_text));
+
+        mSwitchBar = view.findViewById(R.id.switch_bar);
+        Switch switchWidget = mSwitchBar.findViewById(android.R.id.switch_widget);
+        switchWidget.setChecked(enabled);
+        switchWidget.setOnCheckedChangeListener(this);
+        mSwitchBar.setActivated(enabled);
+        mSwitchBar.setOnClickListener(v -> {
+            switchWidget.setChecked(!switchWidget.isChecked());
+            mSwitchBar.setActivated(switchWidget.isChecked());
+        });
+
+        mSmartChargingLevel.setEnabled(enabled);
+        mSmartChargingResumeLevel.setEnabled(enabled);
+        mResetStats.setEnabled(enabled);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.SMART_CHARGING, isChecked ? 1 : 0);
+        mTextView.setText(getString(isChecked ? R.string.switch_on_text : R.string.switch_off_text));
+        mSwitchBar.setActivated(isChecked);
+
+        mSmartChargingLevel.setEnabled(isChecked);
+        mSmartChargingResumeLevel.setEnabled(isChecked);
+        mResetStats.setEnabled(isChecked);
     }
 
     @Override