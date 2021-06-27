405dff5e7b96fa13e708482f7ba50589e551397d
RakeshBatra <raakesh.batra@rediffmail.com>
2021-06-26 20:16:29 -0700

    Settings: enhance the showcase of ColtOS Version
    Credits : https://github.com/Corvus-R/android_packages_apps_Settings/commit/feba9b727a8dead75d87e0e377ebf554e6777a4b#diff-ccc8e1df82741cc2a4b8525fdd40c2076e7c7e9529016585db676bad8c5dd4e6
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/ColtVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/ColtVersionPreferenceController.java
@@ -17,19 +17,27 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.SystemProperties;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 
-import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
 
 import com.android.settings.R;
-import com.android.settings.Utils;
 import com.android.settings.core.BasePreferenceController;
 
 public class ColtVersionPreferenceController extends BasePreferenceController {
 
-    @VisibleForTesting
-    static final String COLT_VERSION_PROPERTY = "ro.colt.version";
+    private static final Uri INTENT_URI_DATA = Uri.parse("https://github.com/Colt-Enigma/");
+    private static final String TAG = "coltDialogCtrl";
+    private static final String COLT_VERSION_PROP = "ro.colt.build.version";
+    private static final String COLT_RELEASETYPE_PROP = "ro.colt.build.type";
+    private static final String COLT_CODENAME_PROP = "ro.colt.device";
+    private final PackageManager mPackageManager = this.mContext.getPackageManager();
 
     public ColtVersionPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -37,12 +45,35 @@ public class ColtVersionPreferenceController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        return !TextUtils.isEmpty(SystemProperties.get(COLT_VERSION_PROPERTY)) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return !TextUtils.isEmpty(SystemProperties.get(COLT_VERSION_PROP)) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
     public CharSequence getSummary() {
-        return SystemProperties.get(COLT_VERSION_PROPERTY,
+	String coltVersion = SystemProperties.get(COLT_VERSION_PROP,
                 mContext.getString(R.string.device_info_default));
+        String coltReleasetype =  SystemProperties.get(COLT_RELEASETYPE_PROP,
+                this.mContext.getString(R.string.device_info_default));
+        String coltCodename = SystemProperties.get(COLT_CODENAME_PROP,
+                mContext.getString(R.string.device_info_default));
+        if (!coltVersion.isEmpty() && !coltReleasetype.isEmpty())
+            return coltVersion + " | " + coltCodename + " | " + coltReleasetype;
+        else
+            return mContext.getString(R.string.colt_version_default);
+    }
+
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            return false;
+        }
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        intent.setData(INTENT_URI_DATA);
+        if (this.mPackageManager.queryIntentActivities(intent, 0).isEmpty()) {
+            Log.w(TAG, "queryIntentActivities() returns empty");
+            return true;
+        }
+        this.mContext.startActivity(intent);
+        return true;
     }
 }
