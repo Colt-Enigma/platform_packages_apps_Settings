Project-Xtended/packages_apps_Settings

Settings: GamingMode: Bringup to Android S [2/4] We currently have no plans to add our own custom settings center, so the current gaming  mode is arranged in the system settings sub-item.
Due to API changes, I currently cannot solve the
problem of reading and writing the floating menu
settings in the gaming mode XD

Change-Id: I284d5371d8e2fe2a0327200a1dffaa784c65a334
Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

src/com/android/settings/preferences/game/DanmakuSettings.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2021 The exTHmUI Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.preferences.game;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class DanmakuSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.exthm_settings_gaming_danmaku);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENDED;
+    }
+}

src/com/android/settings/preferences/game/GamingModeSettings.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2021 The exTHmUI Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.preferences.game;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.preference.SwitchPreference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.ArrayList;
+
+import com.android.settings.preferences.PackageListPreference;
+
+public class GamingModeSettings extends SettingsPreferenceFragment {
+
+    private PackageListPreference mGamingPrefList;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.exthm_settings_gaming);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mGamingPrefList = (PackageListPreference) findPreference("gaming_mode_app_list");
+        mGamingPrefList.setRemovedListKey(Settings.System.GAMING_MODE_REMOVED_APP_LIST);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENDED;
+    }
+}

src/com/android/settings/preferences/game/QuickStartAppSettings.java
@@ -1,0 +1,62 @@
+/*
+ * Copyright (C) 2021 The exTHmUI Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.preferences.game;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class QuickStartAppSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.exthm_settings_gaming_qs_app);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENDED;
+    }
+}

Colt-Enigma/platform_packages_apps_Settings

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/settings/preferences/game/DanmakuSettings.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2021 The exTHmUI Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.preferences.game;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class DanmakuSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.exthm_settings_gaming_danmaku);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.COLT;
+    }
+}

src/com/android/settings/preferences/game/GamingModeSettings.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2021 The exTHmUI Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.preferences.game;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.preference.SwitchPreference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.ArrayList;
+
+import com.android.settings.preferences.PackageListPreference;
+
+public class GamingModeSettings extends SettingsPreferenceFragment {
+
+    private PackageListPreference mGamingPrefList;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.exthm_settings_gaming);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mGamingPrefList = (PackageListPreference) findPreference("gaming_mode_app_list");
+        mGamingPrefList.setRemovedListKey(Settings.System.GAMING_MODE_REMOVED_APP_LIST);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.COLT;
+    }
+}

src/com/android/settings/preferences/game/QuickStartAppSettings.java
@@ -1,0 +1,62 @@
+/*
+ * Copyright (C) 2021 The exTHmUI Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.preferences.game;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class QuickStartAppSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.exthm_settings_gaming_qs_app);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.COLT;
+    }
+}