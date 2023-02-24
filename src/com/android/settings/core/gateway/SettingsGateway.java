Evolution-X-AOSP/packages_apps_Settings

Settings: Add Security Hub from Pixel devices
Obviously it needs the SecurityHubPrebuilt.apk installed
on your system for it to work correctly.

Change-Id: I3843f6134743db9802bc5c84bc4f609928549081
Signed-off-by: Jason Edson <jaysonedson@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -183,6 +183,8 @@ import com.android.settings.livedisplay.LiveDisplaySettings;
 import com.evolution.settings.EvolutionSettings;
 import com.evolution.settings.fragments.HeadsUpSettings;
 
+import com.google.android.settings.security.SecurityHubDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -352,7 +354,8 @@ public class SettingsGateway {
             TurnScreenOnDetails.class.getName(),
             EvolutionSettings.class.getName(),
             HeadsUpSettings.class.getName(),
-            LiveDisplaySettings.class.getName()
+            LiveDisplaySettings.class.getName(),
+            SecurityHubDashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

Colt-Enigma/platform_packages_apps_Settings

Signed-off-by: aswin7469 <aswinas@pixysos.com>
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -183,6 +183,8 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.colt.enigma.ColtEnigma;
 import ink.kscope.settings.wifi.tether.WifiTetherClientManager;
 
+import com.google.android.settings.security.SecurityHubDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -352,7 +354,8 @@ public class SettingsGateway {
             TurnScreenOnDetails.class.getName(),
             ColtEnigma.class.getName(),
             RunningServices.class.getName(),
-            WifiTetherClientManager.class.getName()
+            WifiTetherClientManager.class.getName(),
+            SecurityHubDashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {