60f6bd5e5d3e4f3b1ac01f5a76a15b6fc1cf58bd
RakeshBatra <raakesh.batra@rediffmail.com>
2023-03-05 05:42:46 +0000

    settings: corrections

--- src/com/android/settings/sound/CustomVibrationPreferenceFragment.java
+++ src/com/android/settings/sound/CustomVibrationPreferenceFragment.java
@@ -34,7 +34,7 @@ public class CustomVibrationPreferenceFragment extends DashboardFragment {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EVO_SETTINGS;
+        return MetricsEvent.COLT;
     }
 
     @Override
